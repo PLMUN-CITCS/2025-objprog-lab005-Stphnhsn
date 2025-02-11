bd984ae98c5f04c0297aa96bb643f379cb0879d9
Stphnhsn <stephanhusain02@gmail.com>
2025-02-11 16:41:54 +0800

    Last Sync: 2025-02-11 08:41:54 (Mobile)

---
+++ MathLibraryDemo.java
@@ -0,0 +1,21 @@
+public class MathFunctions {
+    public static void main(String[] args) {
+        
+        double powerResult = Math.pow(2, 3);
+
+        
+        double squareRootResult = Math.sqrt(16);
+
+        
+        long roundedResult = Math.round(5.6);
+
+        
+        double randomNumber = Math.random();
+
+        
+        System.out.println("Power: " + powerResult);
+        System.out.println("Square Root: " + squareRootResult);
+        System.out.println("Rounded Number: " + roundedResult);
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
